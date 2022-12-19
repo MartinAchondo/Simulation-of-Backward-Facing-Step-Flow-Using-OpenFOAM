8bdd3838c6acbce542d0d309cf6aca2f04a1cc4a
martinachondo <acho.martin@gmail.com>
2022-12-19 03:56:49 -0300

    mod

--- 2d_k_omega/fieldsValues.py
+++ 2d_k_omega/fieldsValues.py
@@ -1,14 +1,27 @@
 
 import os
 
-mod_line = 20
-file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'0')
+text = '''/*--------------------------------*- C++ -*----------------------------------*\\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Version:  8
+     \\/     M anipulation  |
+---------------------------------------------------------------------------
+Description
+    Values of fields and properties.
+
+\*---------------------------------------------------------------------------*/\n\n\n'''
+
+
+
+file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'constant')
 
 Re = 8000
 H = 0.2
 h = 0.1
-nu = 10**-6
-rho = 10**3
+nu = 10.0**-6
+rho = 10.0**3
 L = 0.07*h
 
 T = 0.05
@@ -23,35 +36,35 @@ omega = epsilon/k
 factor = 1/(0.5*u**2)
 
 
-def modify_file(name,text,mod_line):
-    with open(os.path.join(file_path,name), 'r') as f:
-        content = f.read()
-        content = content.split('\n')
-        content[mod_line-1] = text
-        content = '\n'.join(content)
+def modify_file(name,text):
+    # with open(os.path.join(file_path,name), 'r') as f:
+    #     content = f.read()
+    #     content = content.split('\n')
+    #     content[mod_line-1] = text
+    #     content = '\n'.join(content)
     with open(os.path.join(file_path,name), 'w') as f:
-        f.write(content)
+        f.write(text)
   
-name = 'U'
-text = f'                const scalar U_inlet = {u};'
-modify_file(name,text,56)
 
-name = 'k'
-text = f'internalField   uniform {k};'
-modify_file(name,text,mod_line)
+name = 'initialConditions'
 
-name = 'omega'
-text = f'internalField   uniform {omega};'
-modify_file(name,text,mod_line)
+text += f'U_inlet_ave   {u};\n'
+text += f'k_ave         {k};\n'
+text += f'omega_ave     {omega};\n'
+text += f'nu_input      {nu};\n'
+text += f'rho_input     {rho};\n'
+text += f'factor_adim   {factor};\n'
+
+modify_file(name,text)
 
 
 
-file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'system')
-mod_line = 18
+# file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'system')
+# mod_line = 18
 
-name = 'controlDict'
-text = f'scale_factor  {factor};'
-modify_file(name,text,mod_line)
+# name = 'controlDict'
+# text = f'scale_factor  {factor};'
+# modify_file(name,text,mod_line)
 
 
 print("Fields Values OK\n\n")
